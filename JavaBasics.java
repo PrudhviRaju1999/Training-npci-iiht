1f9a3c1a41aa785eb31d96ee1d853a6ec9284fb7
PrudhviRaju1999 <prudhvi1209@hotmail.com>
2021-09-21 12:00:53 +0530

    java conditionals

---
+++ JavaBasics.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class JavaBasics{
+
+public static void main(String[] args)
+{
+    int prev=0;
+    int firstval=0;
+    Scanner sc=new Scanner(System.in);
+    firstval=sc.nextInt();
+    if(firstval==prev*prev)
+    {
+        if(firstval%prev==0)
+        {
+            System.out.println("it is a square and multiple of %d "+(firstval/prev));
+        }
+        else
+        {
+            System.out.println("its nly a square");
+        }
+    }
+    if(firstval%prev==0)
+    {
+        System.out.println("it is a square and multiple of %d "+(firstval/prev));
+    }
+    else{
+        System.out.println("neither square or multiple");
+    }
+}
+
+
+    
+}

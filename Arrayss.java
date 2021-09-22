77aa30571501004e76599e434705bb5ee529d9d0
PrudhviRaju1999 <prudhvi1209@hotmail.com>
2021-09-22 09:59:37 +0530

    Matrix assignment

---
+++ Arrayss.java
@@ -0,0 +1,16 @@
+public class Arrayss
+{
+    public static void main(String[] args) {
+        int arr[]={1,5,6,6,6,7,8,9,9,1};
+        int a=arr.length;
+int ar[]=new int[a];
+        for(int i=0;i<=a;i++){
+            for(int x=i+1;x<=a;x++)
+            {
+
+            }
+
+
+        }
+    }
+}

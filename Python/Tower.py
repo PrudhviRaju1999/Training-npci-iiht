41dcd7b18c05f01a1a7327860353ce0bf8c8883b
PrudhviRaju1999 <90675358+PrudhviRaju1999@users.noreply.github.com>
2021-09-27 15:49:09 +0530

    Update Tower.py
    Asssignment 3

--- Python/Tower.py
+++ Python/Tower.py
@@ -1,20 +1,56 @@
-n=5
+n=3
 print("tower problem")
 i=0
+
+y=[]
+z=[]
 while i<=n:
     j=1
+    x=0
+    sum=0
     while j<=i:
-        print(2*i-1,end=" ")
+        print(3*i-2+x,end=" ")
+        z.append(3*i-2+x)
+        sum=sum+ 3*i-2+x
         j=j+1
+        x=x+1
     i=i+1
     print()
+    y.append(sum)
+
+print(y)
+print(z)
+
+
 
 i=1
-j=1
-while i<=n:
-    x=2*i-1
-    y=x*j
-    print("row ",i," ",y)
-    j=j+1
-    i=i+1
 
+while i<=n:
+    print("Row ",i,"value is",y[i])
+    i=i+1
+sum=0
+for i in range(len(z)): 
+    sum=z[i]+sum
+print("Row last value is",sum)
+print("create Matrix")
+
+z1=z[3:]
+l2=[0,0]
+
+z2=z[1:3]
+z2.append(0)
+z3=z[0:1]
+z3=z3+l2
+print(z1)
+print(z2)
+print(z3)
+
+
+
+for i in range(len(z1)): 
+    z1[i]=z1[i]**2
+    z2[i]=z2[i]**2 
+    z3[i]=z3[i]**2  
+print(z1) 
+print(z2)
+print(z3)

35fcbb5e5afa98255a12ea5eeae53197f6c9cf51
PrudhviRaju1999 <90675358+PrudhviRaju1999@users.noreply.github.com>
2021-09-28 13:10:18 +0530

    All Assignments 1, 2, 3

--- Python/setoperations.py
+++ Python/setoperations.py
@@ -15,3 +15,23 @@ while ls_list:
 
 print(nw)
 
+#asssignment 2
+
+s1="mad"
+s2="bam"
+s3="adm"
+
+if sorted(s1)==sorted(s2):
+    if sorted(s1)==sorted(s3):
+        print("Yes 3 strings are anagrams")
+else:
+    print("Not anagramas") 
+
+
+#Assignment 3
+list_ls=[1,2,3,4,7,8,9]
+
+for i in range(list_ls[0],list_ls[-1]+1):
+    if i not in list_ls:
+     print(i)
+
